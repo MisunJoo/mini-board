026d94f85424c8af12d7b8a766b51edb28167c51
Gyumin Kim <starkying@gmail.com>
2018-10-11 14:05:41 +0900

    로그인 예외처리: 없는 ID인 경우 & 비밀번호가 틀린 경우 => form 하단에 안내 메시지 출력

--- src/main/java/my/examples/miniboard/servlet/MiniboardLoginServlet.java
+++ src/main/java/my/examples/miniboard/servlet/MiniboardLoginServlet.java
@@ -39,17 +39,27 @@ public class MiniboardLoginServlet extends HttpServlet {
         User user = userDao.getUser(userName);
         System.out.println("로그인시도 => userName: " + userName + ", password: " + password);
 
-        // 비밀번호 일치하지 않을 경우, login 페이지로 redirect
-        if (!user.getPassword().equals(password)) {
-            resp.sendRedirect("/miniboard/login");
+        // User 테이블에 해당 name을 갖는 user가 없는 경우
+        if (user == null) {
+            req.setAttribute("isIdNull", true);
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/login.jsp");
+            dispatcher.forward(req, resp);
         }
-        // 비밀번호 일치할 경우, session에 user 정보 set하고 index로 redirect
         else {
-            System.out.println("getUser(userName)으로 가져온 user 객체의 userName: " + user.getUserName());
-            System.out.println("getUser(userName)으로 가져온 user 객체의 password: " + user.getPassword());
-            HttpSession httpSession = req.getSession();
-            httpSession.setAttribute("authUser", user);
-            resp.sendRedirect("/"); // index로 redirect
+            // 비밀번호 일치하지 않을 경우
+            if (!user.getPassword().equals(password)) {
+                req.setAttribute("wrongPw", true);
+                RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/login.jsp");
+                dispatcher.forward(req, resp);
+            }
+            // 비밀번호 일치할 경우(login 성공), session에 user 정보 set하고 index로 redirect
+            else {
+                System.out.println("getUser(userName)으로 가져온 user 객체의 userName: " + user.getUserName());
+                System.out.println("getUser(userName)으로 가져온 user 객체의 password: " + user.getPassword());
+                HttpSession httpSession = req.getSession();
+                httpSession.setAttribute("authUser", user);
+                resp.sendRedirect("/"); // index로 redirect
+            }
         }
     }
 }
