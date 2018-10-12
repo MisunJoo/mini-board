bf92ac0b5834e7fd9d7e5db107445ee07f544b77
MisunJoo <0987someday@naver.com>
2018-10-12 23:22:29 +0900

    list 분류 (수정중)

--- src/main/java/my/examples/miniboard/servlet/MiniboardListServlet.java
+++ src/main/java/my/examples/miniboard/servlet/MiniboardListServlet.java
@@ -2,6 +2,7 @@ package my.examples.miniboard.servlet;
 
 import my.examples.miniboard.dao.ArticleDao;
 import my.examples.miniboard.dto.Article;
+import my.examples.miniboard.dto.User;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,22 +10,46 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
 @WebServlet("/miniboard/list")
 public class MiniboardListServlet extends HttpServlet {
+
+    String country;
+    String category;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ArticleDao articleDao = new ArticleDao();
-        List<Article> articles = articleDao.getArticleList();
+        List<Article> articles = articleDao.getArticleList(country, category);
 
         req.setAttribute("articleList", articles);
         req.setAttribute("articleListSize", articles.size());
 
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/views/list.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("authUser");
+
+        country = req.getParameter("country");
+        category = req.getParameter("category");
+
+//        ArticleDao articleDao = new ArticleDao(country, category);
+
+        ArticleDao articleDao = new ArticleDao();
 
 
-
+        //국가에 따른 리스틀 불러옴
+        List<Article> list = articleDao.getArticleList(country, category);
+        req.setAttribute("article", list);
 
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/views/list.jsp");
         requestDispatcher.forward(req, resp);
--- webapp/WEB-INF/views/list.jsp
+++ webapp/WEB-INF/views/list.jsp
@@ -5,24 +5,28 @@
     <title>Title</title>
 </head>
 <body>
-<select name="country" required>
-    <!--<option value="" selected disabled hidden>음식 종류</option>-->
-    <option value="korean">한식</option>
-    <option value="chinese">중식</option>
-    <option value="korean">양식</option>
-    <option value="western">일식</option>
-</select>
-<select name="category" required>
-    <!--<option value="" selected disabled hidden>글 종류</option>-->
-    <option value="promotion">홍보</option>
-    <option value="review">후기</option>
-</select>
+
+<form method="post" action="./list.jsp">
+    <select name="country">
+        <!--<option value="" selected disabled hidden>음식 종류</option>-->
+        <option value="korean">한식</option>
+        <option value="chinese">중식</option>
+        <option value="korean">양식</option>
+        <option value="western">일식</option>
+    </select>
+    <select name="category">
+        <!--<option value="" selected disabled hidden>글 종류</option>-->
+        <option value="promotion">홍보</option>
+        <option value="review">후기</option>
+    </select>
+</form>
 글번호 제목 작성자 등록일
 <br>
 
 
 <!-- 게시글 목록을 출력한다. -->
 <c:forEach items="${requestScope.articleList}" var="article">
+
     ${article.country} &nbsp;
     ${article.category} &nbsp;
     ${article.id} &nbsp;
